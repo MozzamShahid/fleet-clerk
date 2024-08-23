cf3d5f7a17a827d1737f0d3aa4d5514ecacc98af
Mozzam Shahid <mozzam@Mozzams-MacBook-Air.local>
2024-08-23 15:53:11 +0500

    hello

--- src/pages/Driverd.jsx
+++ src/pages/Driverd.jsx
@@ -1,90 +1,180 @@
-import React from 'react'
-import ImageUpload from '../components/ImageUpload'
-
+import React, { useState } from 'react';
+import ImageUpload from '../components/ImageUpload';
 
 const Driverd = () => {
+  // State for form inputs
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    phoneNumber: '',
+    emergencyPhoneNumber: '',
+    nationalId: '',
+    drivingLicense: '',
+  });
+
+  // State for uploaded images
+  const [uploadedImages, setUploadedImages] = useState({
+    idCard: null,
+    drivingLicense: null
+  });
+
+  // Handle input changes
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  // Handle image uploads
+  const handleImageUpload = (event, imageType) => {
+    const file = event.target.files[0];
+    if (file) {
+      setUploadedImages(prevState => ({
+        ...prevState,
+        [imageType]: URL.createObjectURL(file)
+      }));
+    }
+  };
+
+  // Handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form Data:', formData);
+    console.log('Uploaded Images:', uploadedImages);
+  };
+
   return (
-    <div className='main  bg-white flex flex-col'>
+    <div className='main bg-white flex flex-col'>
       <h1 className='text-4xl bg-blue-500 text-white p-3 rounded-sm'>Drivers Details</h1>
 
       <div className='form'>
-        <form >
+        <form onSubmit={handleSubmit}>
           <div className='image flex justify-start p-5'>
-            <ImageUpload />
+            <label className='uploadid-label lab-text'>Upload ID Card Image:</label>
+            <input
+              type="file"
+              accept="image/*"
+              onChange={(e) => handleImageUpload(e, 'idCard')}
+            />
+            {uploadedImages.idCard && (
+              <img src={uploadedImages.idCard} alt="ID Card" className='uploaded-image' />
+            )}
           </div>
+
           <div className='flex mt-3 gap-10'>
-            {/* this is first name and last name in flex property side by side */}
             <div className='flex flex-col gap-2 text-left'>
-              <label class='first-namelabel lab-text'> First Name </label>
-              <input class='first-nameinput custom-input w-96' type="text" placeholder='Mozzam' />
+              <label className='first-namelabel lab-text'>First Name</label>
+              <input
+                name="firstName"
+                className='first-nameinput custom-input w-96'
+                type="text"
+                placeholder='Mozzam'
+                value={formData.firstName}
+                onChange={handleChange}
+              />
             </div>
 
             <div className='flex flex-col gap-2 text-left'>
-              <label class='last-namelabel lab-text'>Last Name</label>
-              <input class='last-nameinput custom-input w-96' type="text" placeholder='Shahid' />
+              <label className='last-namelabel lab-text'>Last Name</label>
+              <input
+                name="lastName"
+                className='last-nameinput custom-input w-96'
+                type="text"
+                placeholder='Shahid'
+                value={formData.lastName}
+                onChange={handleChange}
+              />
             </div>
           </div>
 
           <div className='email flex flex-col gap-2 text-left mt-3'>
-            <label class='email-label lab-text'>Email:</label>
-            <input class='email-input custom-input w-96' type="email" placeholder='mozzamshahid@gmail.com' />
+            <label className='email-label lab-text'>Email:</label>
+            <input
+              name="email"
+              className='email-input custom-input w-96'
+              type="email"
+              placeholder='mozzamshahid@gmail.com'
+              value={formData.email}
+              onChange={handleChange}
+            />
           </div>
-          {/* this is phone number and emergency phone number in flex property side by side */}
+
           <div className='flex mt-3 gap-10'>
-
             <div className='flex flex-col gap-2 text-left'>
-              <label class='phone-label lab-text'>Phone Number</label>
-              <input class='phone-input custom-input w-96' type="phone" placeholder='+68' />
+              <label className='phone-label lab-text'>Phone Number</label>
+              <input
+                name="phoneNumber"
+                className='phone-input custom-input w-96'
+                type="text"
+                placeholder='+68'
+                value={formData.phoneNumber}
+                onChange={handleChange}
+              />
             </div>
 
             <div className='flex flex-col gap-2 text-left'>
-              <label class='ephone-label lab-text'>Emergency Phone Number</label>
-              <input class='ephone-input custom-input w-96' type="phone" placeholder='+68' />
+              <label className='ephone-label lab-text'>Emergency Phone Number</label>
+              <input
+                name="emergencyPhoneNumber"
+                className='ephone-input custom-input w-96'
+                type="text"
+                placeholder='+68'
+                value={formData.emergencyPhoneNumber}
+                onChange={handleChange}
+              />
             </div>
           </div>
 
-          {/* this is national id number and driving lisence in flex property side by side */}
           <div className='flex mt-3 gap-10'>
+            <div className='flex flex-col gap-2 text-left'>
+              <label className='id-label lab-text'>National ID Number</label>
+              <input
+                name="nationalId"
+                className='id-input custom-input w-96'
+                type="text"
+                placeholder='Your National ID Number'
+                value={formData.nationalId}
+                onChange={handleChange}
+              />
+            </div>
 
             <div className='flex flex-col gap-2 text-left'>
-              <label class='id-label lab-text'>National ID  Number</label>
-              <input class='id-input custom-input w-96' type="number" placeholder='Your National ID Number' />
-            </div>
-
-            <div className='flex flex-col gap-2 text-left'>
-              <label class='d-name lab-text'>Driving License Number</label>
-              <input class='d-input custom-input w-96' type="number" placeholder='Your Driving License Number' />
-            </div>
-          </div>
-
-          <div className=' flex flex-col gap-2 text-left mt-3'>
-            <label class='uploadid-label lab-text'>Upload ID Card Image:</label>
-            <div className='image flex justify-start p-5'>
-              <ImageUpload
+              <label className='d-name lab-text'>Driving License Number</label>
+              <input
+                name="drivingLicense"
+                className='d-input custom-input w-96'
+                type="text"
+                placeholder='Your Driving License Number'
+                value={formData.drivingLicense}
+                onChange={handleChange}
               />
-
             </div>
           </div>
 
-          <div className=' flex flex-col gap-2 text-left mt-3'>
-            <label class='drivingid-label lab-text'>Upload Driving Lisence Image:</label>
-            <div className='image flex justify-start p-5'>
-              <ImageUpload
-              />
-
-            </div>
+          <div className='flex flex-col gap-2 text-left mt-3'>
+            <label className='drivingid-label lab-text'>Upload Driving License Image:</label>
+            <input
+              type="file"
+              accept="image/*"
+              onChange={(e) => handleImageUpload(e, 'drivingLicense')}
+            />
+            {uploadedImages.drivingLicense && (
+              <img src={uploadedImages.drivingLicense} alt="Driving License" className='uploaded-image' />
+            )}
           </div>
 
-          <input className='bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-500 w-full' type="submit" />
-
-
-
+          <input
+            className='bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-500 w-full'
+            type="submit"
+            value="Submit"
+          />
         </form>
-
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Driverd
+export default Driverd;
